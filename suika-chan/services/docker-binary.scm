f1283831e5434686286c05e4d7cb10416c64f4f7
SecondCosmicV <secondcosmicv@gmail.com>
2026-06-05 17:04:07 +0200

    provide docker with iptables

--- suika-chan/services/docker-binary.scm
+++ suika-chan/services/docker-binary.scm
@@ -1,4 +1,5 @@
 (define-module (suika-chan services docker-binary)
+  #:use-module (gnu packages linux)
   #:use-module (gnu services)
   #:use-module (gnu services shepherd)
   #:use-module (gnu system shadow)
@@ -19,7 +20,9 @@
     (start #~(make-forkexec-constructor
       (list (string-append #$docker-binary "/bin/dockerd"))
       #:environment-variables (list
-        (string-append "PATH=" #$docker-binary "/bin"))
+        (string-append "PATH="
+          #$docker-binary "/bin:"
+          #$iptables "/sbin"))
       #:pid-file "/var/run/docker.pid"
       #:log-file "/var/log/docker.log"))
     (stop #~(make-kill-destructor))))
@@ -28,10 +31,8 @@
     (name 'docker-binary)
     (description "Provide capability to run Docker application bundles in Docker containers (binary).")
     (extensions (list
-      (service-extension profile-service-type (const (list
-        docker-binary)))
-      (service-extension shepherd-root-service-type (const (list
-        docker-binary-shepherd-service)))
+      (service-extension profile-service-type (const (list docker-binary)))
+      (service-extension shepherd-root-service-type (const (list docker-binary-shepherd-service)))
       (service-extension account-service-type (const (list
         (user-group
           (name "docker")

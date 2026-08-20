c1e1d5b41e2174599a796b1764fcd06b22f4014f
SecondCosmicV <secondcosmicv@gmail.com>
2026-08-20 22:30:35 +0200

    make sure one can ssh into devcont from vscode

--- suika-chan/systems/devcontainer.scm
+++ suika-chan/systems/devcontainer.scm
@@ -1,17 +1,29 @@
 (define-module (suika-chan systems devcontainer)
   #:use-module (gnu)
+  #:use-module (gnu packages commencement)
   #:use-module (gnu services ssh))
 (define-public devcontainer-operating-system
   (operating-system
     (host-name "compose")
-    (services (cons
+    (users (cons
+      (user-account
+        (name "app")
+        (group "users"))
+      %base-user-accounts))
+    (packages (cons
+      gcc-toolchain
+      %base-packages))
+    (services (cons*
       (service openssh-service-type (openssh-configuration
         (port-number 2222)
         (password-authentication? #f)
         (challenge-response-authentication? #f)
-        (permit-root-login #t)
         (authorized-keys `(
-          ("root" ,(local-file (in-vicinity (getcwd) "authorized_keys")))))))
+          ("app" ,(local-file (in-vicinity (getcwd) "authorized_keys")))))))
+      (simple-service 'my-session-environment-service session-environment-service-type '(
+        ("LD_LIBRARY_PATH" . "/lib64")))
+      (extra-special-file "/lib64" "/run/current-system/profile/lib")
+      (extra-special-file "/usr/lib64" "/lib64")
       %base-services))
     (file-systems %base-file-systems)
     (bootloader (bootloader-configuration
